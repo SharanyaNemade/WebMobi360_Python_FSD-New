8faf4d1b0413afe9f1e9d7aacba0622763f215ce
SharanyaNemade <sharanyanemade123@gmail.com>
2026-01-05 13:56:40 +0530

    Initial commit

--- db.py
+++ db.py
@@ -1,10 +1,25 @@
+# import mysql.connector
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="",          # XAMPP default
+#         database="fraud_detection_database",
+#         port=3306
+#     )
+
+
+
+
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",          # XAMPP default
-        database="fraud_detection_database",
-        port=3306
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
