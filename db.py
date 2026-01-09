ded82be8118535233d460787153bda7867d36d10
SharanyaNemade <sharanyanemade123@gmail.com>
2026-01-09 12:28:53 +0530

    first commit

--- db.py
+++ db.py
@@ -1,15 +1,24 @@
+# FOR SYSTEM TESTING ONLY
+
+
+
+
 # import mysql.connector
 
 # def get_db_connection():
 #     return mysql.connector.connect(
 #         host="localhost",
 #         user="root",
-#         password="",          # XAMPP default
+#         password="root",          # XAMPP default
 #         database="fraud_detection_database",
 #         port=3306
 #     )
 
 
+
+
+
+        # FOR DEPLOYMENT ONLY
 
 
 import os
@@ -18,8 +27,34 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
+        ssl_disabled=False
     )
+
+
+
+
+
+
+
+
+
+
+
+#   OPTIONAL
+
+
+# import mysql.connector
+# import os
+
+# def get_db_connection():
+#     return mysql.connector.connect(
+#         host=os.getenv("DB_HOST"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASSWORD"),
+#         database=os.getenv("DB_NAME"),
+#         port=int(os.getenv("DB_PORT", 3306))
+#     )
