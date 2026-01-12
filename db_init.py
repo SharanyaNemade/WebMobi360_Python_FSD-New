03132169b97f6795eca989d6c1d1b9e16acef8aa
SharanyaNemade <sharanyanemade123@gmail.com>
2026-01-12 12:23:40 +0530

    Initial commit

--- db_init.py
+++ db_init.py
@@ -38,15 +38,31 @@
 
 
 
+import os
 import mysql.connector
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
 
 def init_database():
     # connect WITHOUT database first
     conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", 3306)),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
+
+
+        # host=os.getenv("DB_HOST"),
+        # user=os.getenv("DB_USER"),
+        # password=os.getenv("DB_PASSWORD")
+    
     cursor = conn.cursor()
 
     with open("db_init.sql", "r") as f:
